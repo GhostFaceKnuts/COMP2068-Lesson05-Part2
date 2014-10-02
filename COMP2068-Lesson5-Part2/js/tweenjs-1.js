b46f79146c8435edbdbc8def7c22c19887ac65f8
tsiliopoulos <tsiliopoulos@hotmail.com>
2014-10-02 08:41:54 -0400

    Added SoundJS

---
+++ COMP2068-Lesson5-Part2/js/tweenjs-1.js
@@ -0,0 +1,27 @@
+﻿var stage;
+var count = 0;
+function init() {
+    stage = new createjs.Stage(document.getElementById('myCanvas'));
+    createjs.Ticker.addEventListener("tick", handleTick);
+    createjs.Ticker.setFPS(60);
+    start();
+}
+function handleTick(e) {
+    stage.update();
+}
+function start() {
+    drawButterflies();
+}
+function drawButterflies() {
+    var imgPath = 'images/butterfly.png';
+    butterfly = new createjs.Bitmap(imgPath);
+    stage.addChild(butterfly);
+    stage.update();
+    setTimeout(tweenButterfly, 100);
+}
+function tweenButterfly() {
+    createjs.Tween.get(butterfly).to({ y: 100 }, 1000).call(butterflyGone, [butterfly], this);
+}
+function butterflyGone(butterfly) {
+    stage.removeChild(butterfly);
+}
